811998b0a6dc2979db8bbae9b134686072997154
hajjm200 <166666792+hajjm200@users.noreply.github.com>
2025-07-09 03:13:48 +0000

    This the structures code from Module 3 - CS 374

--- structures.c
+++ structures.c
@@ -1,1 +1,24 @@
 
+#include <stdio.h>
+
+int main(void) {
+
+    struct student{
+        char* name;
+        int studentId;
+        char* major;
+    };
+
+ // John Doe
+struct student JohnDoe;
+JohnDoe.name = "John Doe";
+JohnDoe.studentId = 93;
+JohnDoe.major = "Computer Science";
+printf("You are %s , your Student Id is %d, and your major is in %s \n", JohnDoe.name, JohnDoe.studentId, JohnDoe.major);
+
+// We can initialize a struct by providing the element in {}
+struct student JaneDoe = {"Jane Doe", 33, "Civil Engineering"};
+printf("You are %s , your Student Id is %d, and your major is in %s \n", JaneDoe.name, JaneDoe.studentId, JaneDoe.major);
+
+    return 0;
+}
