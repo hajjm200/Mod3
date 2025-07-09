457ade7454d0dd4d305b38dee27febb0b6883861
hajjm200 <166666792+hajjm200@users.noreply.github.com>
2025-07-09 03:40:21 +0000

    This is pointers code from Module 3 - CS 374

--- pointers.c
+++ pointers.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
 
+int main(void) {
+  int i;
+  int* p;
+
+  // Store 1 in the variable i
+  // i is an integree
+  i = 1;
+
+  // Store the address of variable i in variable p
+  // p is andress of i
+  p = &i;
+
+  // The address of i
+
+printf("The adress of i is %p \n", (void*) &i);
+
+  // The value of p, which is the same as the address printed by the previous statement
+
+printf("The value of p is %p \n", (void*) p);
+
+  // Print the value at the address stored in p, i.e., the value of i
+printf("THe value  at the adress stored in p (value of i) is %d \n", *p);
+
+  // Increment the value at the address stored in p. This will increment the value of i
+
+  *p = *p + 1;
+  // Print the value at the address stored in p, i.e., the value of i
+printf("THh value  at the adress stored in p (value of i) after incrementation is %d \n", *p);
+
+  // To make sure, let's print the value of i. It is now 2
+printf ("The value of i after incrementation is %d \n", i);
+  return 0;
+}
